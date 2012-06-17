638642f6d23d1124683634177932deac633109b5
Dave Gallant <dtgallant@gmail.com>
2012-06-17 00:23:45 -0400

    finished basic sign-in-out stuff

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -42,13 +42,16 @@ describe "User pages" do
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      
+      
       end
       
       describe "after saving the user" do
-      	
-      	it { should have_link('Sign out') }
+      	before { click_button submit }
+	
+	    	it { should have_link('Sign out') }
 
-      end
+  	  end
       
       
     end

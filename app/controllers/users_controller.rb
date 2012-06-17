5556f93e8df4147811533e47883fb287215929c8
Dave Gallant <dtgallant@gmail.com>
2012-06-17 00:11:37 -0400

    almost done with user sign-in sign-out demo setup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       # Handle a successful save.
+      sign_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
@@ -23,5 +24,6 @@ class UsersController < ApplicationController
     end
   end
   
-    
+
+	    
 end

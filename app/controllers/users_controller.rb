640f5de35f317948b6c57eb95d567c54c41f13ff
Dave Gallant <dtgallant@gmail.com>
2012-06-18 00:19:48 -0400

    working on update-users section, 1/2 done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :correct_user,   only: [:edit, :update]
+
+
 
   def show
     @user = User.find(params[:id])
@@ -24,6 +28,30 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
 
-	    
+  end
+  
+  def update
+
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end  
+    
+  private
+
+    def signed_in_user
+      redirect_to signin_path, notice: "Please sign in." unless signed_in?
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end    
+    	    
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -17,7 +17,12 @@ module SessionsHelper
   def current_user
     @current_user ||= User.find_by_remember_token(cookies[:remember_token])
   end
+
+  def current_user?(user)
+    user == current_user
+  end
   
+    
   def sign_out
     self.current_user = nil
     cookies.delete(:remember_token)
